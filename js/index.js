2e53a2d7d3cdf25fbfabb4ed2d94d99bc924c899
Evan Xd <itoyxd@gmail.com>
2015-06-17 00:06:27 +0800

    Turn on/off the LED lamp

--- js/index.js
+++ js/index.js
@@ -3,11 +3,22 @@
 
 (function() {
   var webArduino = document.querySelector('#webArduino');
+  var power = document.querySelector('#power');
+
   webArduino.addEventListener('load', function webArduinoOnLoad() {
     webArduino.removeEventListener('load', webArduinoOnLoad);
     var arduino = new Arduino({ address: 'e1:09:43:ea:dd:68' });
     arduino.on('connected', function() {
       arduino.d7 = Arduino.HIGH;
+      power.disabled = false;
+    });
+
+    power.addEventListener('click', function() {
+      if (arduino.d7) {
+        arduino.d7 = Arduino.LOW;
+      } else {
+        arduino.d7 = Arduino.HIGH;
+      }
     });
   });
 }());
