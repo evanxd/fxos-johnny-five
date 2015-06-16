9cd348dc6be0d1ae3197308b10ab16be131a5ffb
Evan Xd <itoyxd@gmail.com>
2015-06-16 23:34:44 +0800

    Wait for Web Arduino loaded, and light the LED lamp

--- js/index.js
+++ js/index.js
@@ -2,9 +2,12 @@
 'use strict';
 
 (function() {
-  var arduino = document.querySelector('#arduino');
-  arduino.d7 = Arduino.HIGH;
-  setInterval(function() {
-    arduino.d7 = !arduino.d7;
-  }, 300);
+  var webArduino = document.querySelector('#webArduino');
+  webArduino.addEventListener('load', function webArduinoOnLoad() {
+    webArduino.removeEventListener('load', webArduinoOnLoad);
+    var arduino = new Arduino({ address: 'e1:09:43:ea:dd:68' });
+    arduino.on('connected', function() {
+      arduino.d7 = Arduino.HIGH;
+    });
+  });
 }());
