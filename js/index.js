bbf027f15f9398ee7b8e5ee7d699d44487f5d94e
Evan Xd <itoyxd@gmail.com>
2015-06-17 10:39:39 +0800

    Listen webarduino-connected event

--- js/index.js
+++ js/index.js
@@ -2,41 +2,38 @@
 'use strict';
 
 (function() {
-  var webArduino = document.querySelector('#webArduino');
   var power = document.querySelector('#power');
   var blink = document.querySelector('#blink');
+  var arduino;
   var blinkTimerID;
 
-  webArduino.addEventListener('load', function webArduinoOnLoad() {
-    webArduino.removeEventListener('load', webArduinoOnLoad);
-    var arduino = new Arduino({ address: 'e1:09:43:ea:dd:68' });
-    arduino.on('connected', function() {
+  window.addEventListener('webarduino-connected', function(evt) {
+    arduino = evt.detail.arduino;
+    arduino.d7 = Arduino.HIGH;
+    power.disabled = false;
+    blink.disabled = false;
+  });
+
+  power.addEventListener('click', function() {
+    if (arduino.d7) {
+      arduino.d7 = Arduino.LOW;
+    } else {
       arduino.d7 = Arduino.HIGH;
-      power.disabled = false;
-      blink.disabled = false;
-    });
+    }
+  });
 
-    power.addEventListener('click', function() {
-      if (arduino.d7) {
-        arduino.d7 = Arduino.LOW;
-      } else {
-        arduino.d7 = Arduino.HIGH;
-      }
-    });
-
-    blink.addEventListener('click', function() {
-      if (blinkTimerID) {
-        clearInterval(blinkTimerID);
-        blinkTimerID = null;
-      } else {
-        blinkTimerID = setInterval(function() {
-          if (arduino.d7) {
-            arduino.d7 = Arduino.LOW;
-          } else {
-            arduino.d7 = Arduino.HIGH;
-          }
-        }, 300);
-      }
-    });
+  blink.addEventListener('click', function() {
+    if (blinkTimerID) {
+      clearInterval(blinkTimerID);
+      blinkTimerID = null;
+    } else {
+      blinkTimerID = setInterval(function() {
+        if (arduino.d7) {
+          arduino.d7 = Arduino.LOW;
+        } else {
+          arduino.d7 = Arduino.HIGH;
+        }
+      }, 300);
+    }
   });
 }());
