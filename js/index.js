7483daef925dbc28168e85d03ed8da1c32e64b90
Evan Xd <itoyxd@gmail.com>
2015-06-17 16:01:46 +0800

    Use getters and setters of digital pins in Web Arduino

--- js/index.js
+++ js/index.js
@@ -5,18 +5,16 @@
   var light = document.querySelector('#light');
   var power = document.querySelector('#power');
   var blink = document.querySelector('#blink');
-  var arduino;
   var blinkTimerID;
 
   light.addEventListener('connected', function(evt) {
-    arduino = evt.detail.arduino;
-    arduino.d7 = Arduino.HIGH;
+    light.d7 = Arduino.HIGH;
     power.disabled = false;
     blink.disabled = false;
   });
 
   power.addEventListener('click', function() {
-    arduino.d7 = !arduino.d7;
+    light.d7 = !light.d7;
   });
 
   blink.addEventListener('click', function() {
@@ -25,8 +23,11 @@
       blinkTimerID = null;
     } else {
       blinkTimerID = setInterval(function() {
-        arduino.d7 = !arduino.d7;
+        light.d7 = !light.d7;
       }, 300);
     }
   });
+
+  // For testing and debugging.
+  window.light = light;
 }());
