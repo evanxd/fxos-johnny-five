9b6777332bbf7e7323190a5196e28645721faf1c
Evan Xd <itoyxd@gmail.com>
2015-06-19 18:54:37 +0800

    Control two LED lamps so easily

--- js/index.js
+++ js/index.js
@@ -2,19 +2,27 @@
 'use strict';
 
 (function() {
-  var light = document.querySelector('#light');
+  var light1 = document.querySelector('#light1');
+  var light2 = document.querySelector('#light2');
   var power = document.querySelector('#power');
   var blink = document.querySelector('#blink');
   var blinkTimerID;
 
-  light.addEventListener('connected', function(evt) {
-    light.d7 = Arduino.HIGH;
+  light1.addEventListener('connected', function(evt) {
+    light1.d7 = Arduino.HIGH;
+    power.disabled = false;
+    blink.disabled = false;
+  });
+
+  light2.addEventListener('connected', function(evt) {
+    light2.d7 = Arduino.HIGH;
     power.disabled = false;
     blink.disabled = false;
   });
 
   power.addEventListener('click', function() {
-    light.d7 = !light.d7;
+    light1.d7 = !light1.d7;
+    light2.d7 = !light2.d7;
   });
 
   blink.addEventListener('click', function() {
@@ -23,11 +31,13 @@
       blinkTimerID = null;
     } else {
       blinkTimerID = setInterval(function() {
-        light.d7 = !light.d7;
+        light1.d7 = !light1.d7;
+        light2.d7 = !light2.d7;
       }, 300);
     }
   });
 
   // For testing and debugging.
-  window.light = light;
+  window.light1 = light1;
+  window.light2 = light2;
 }());
