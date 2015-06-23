0254efe214420565de3a48826672bda5bbe41484
Evan Xd <itoyxd@gmail.com>
2015-06-23 07:41:16 -0700

    Remove Light2

--- js/index.js
+++ js/index.js
@@ -2,21 +2,14 @@
 'use strict';
 
 (function() {
-  var light1 = document.querySelector('#light1');
-  var light2 = document.querySelector('#light2');
+  var light = document.querySelector('#light');
   var buttons = document.querySelector('#buttons');
   var power = document.querySelector('#power');
   var blink = document.querySelector('#blink');
   var blinkTimerID;
 
-  light1.addEventListener('connected', function() {
-    light1.d7 = Arduino.HIGH;
-    power.disabled = false;
-    blink.disabled = false;
-  });
-
-  light2.addEventListener('connected', function() {
-    light2.d7 = Arduino.HIGH;
+  light.addEventListener('connected', function() {
+    light.d7 = Arduino.HIGH;
     power.disabled = false;
     blink.disabled = false;
   });
@@ -27,15 +20,13 @@
 
   buttons.addEventListener('digitalpinchanged', function(evt) {
     var pins = evt.detail.pins;
-    light1.d7 = pins.d7;
+    light.d7 = pins.d7;
     // FIXME: Cannot control more man two devices at same time
     // probably because of BLE APIs.
-    // light2.d7 = pins.d7;
   });
 
   power.addEventListener('click', function() {
-    light1.d7 = !light1.d7;
-    light2.d7 = !light2.d7;
+    light.d7 = !light.d7;
   });
 
   blink.addEventListener('click', function() {
@@ -44,13 +35,11 @@
       blinkTimerID = null;
     } else {
       blinkTimerID = setInterval(function() {
-        light1.d7 = !light1.d7;
-        light2.d7 = !light2.d7;
+        light.d7 = !light.d7;
       }, 300);
     }
   });
 
   // For testing and debugging.
-  window.light1 = light1;
-  window.light2 = light2;
+  window.light = light;
 }());
