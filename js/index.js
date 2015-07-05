cffd14c3812f385c57a9af49abff2fe025496c7d
Evan Xd <itoyxd@gmail.com>
2015-07-05 11:30:28 +0800

    Remove web-arduino tag in index.html

--- js/index.js
+++ js/index.js
@@ -2,30 +2,16 @@
 'use strict';
 
 (function() {
-  var light = document.querySelector('#light');
-  var buttons = document.querySelector('#buttons');
+  var light = new Arduino({ address: 'e1:09:43:ea:dd:68' });
   var power = document.querySelector('#power');
   var blink = document.querySelector('#blink');
   var blinkTimerID;
 
-  light.addEventListener('connected', function() {
+  light.on('connect', function() {
     console.log('The light board is connected.');
     light.d7 = Arduino.HIGH;
     power.disabled = false;
     blink.disabled = false;
-  });
-
-  buttons.addEventListener('connected', function() {
-    console.log('The buttons board is connected.');
-    buttons.subscribe(7);
-  });
-
-  buttons.addEventListener('digitalpinchanged', function(evt) {
-    var pins = evt.detail.pins;
-    light.d7 = pins.d7;
-    // FIXME: Cannot control more man two devices at same time
-    // probably because of BLE APIs.
-    console.log('The pin 7 of buttons board is changed:' + pins.d7);
   });
 
   power.addEventListener('click', function() {
@@ -43,6 +29,6 @@
     }
   });
 
-  // For testing and debugging.
+  // For debugging.
   window.light = light;
 }());
